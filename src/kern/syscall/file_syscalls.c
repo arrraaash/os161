a2f449ce0ac1b2430a44c5db93378ac452b4352d
arrraaash <arash.amini.71@gmail.com>
2023-12-25 16:35:21 +0100

    modify sys_open

--- src/kern/syscall/file_syscalls.c
+++ src/kern/syscall/file_syscalls.c
@@ -16,30 +16,51 @@
 #include <lib.h>
 
 
-int sys_open(const char *filename, int flags, int *retval) {
+int sys_open(const char *filename, int flags, mode_t mode, int *retval) {
 	
-	char cin_filename[PATH_MAX]; 
-	int copyinside = copyinstr((const_userptr_t)filename, cin_filename, len, &got);
-	if (copyinside) {
-		return copyinside;
+	char dest[PATH_MAX];
+	size_t len = PATH_MAX;
+	size_t got;
+	int copyinside = copyinstr(filename, dest , len, &got);  //chacking the validity of the filename
+	if (copyinside) {    //copyinstr returns null-terminated on success
+		return EFAULT;
 	}
-	while(curproc->file_table[i] != NULL){
+	// curproc is always the current thread's process definged in current.h
+	while(curproc->p_ft[i] != NULL){
 		if(i == OPEN_MAX-1){
 			return EMFILE;	
 		}
 		i++;
 	}
-	curproc->file_table[i] = (struct file_handle *)kmalloc(sizeof(struct file_handle));
-	KASSERT(curproc->file_table[i] != NULL);
-	err = vfs_open(cin_filename, flags, 0, &curproc->file_table[i]->vnode);
+	// allocating space and definging the file table
+	curproc->p_ft[i] = (struct file_handle *)kmalloc(sizeof(struct file_handle));
+	KASSERT(curproc->p_ft[i] != NULL);
+	err = vfs_open(dest, flags, 0, &curproc->p_tf[i]->vnode);
+	// destroy the created space and file table in case of error
 	if (err) {
-		kfree(curproc->file_table[i]);
-		curproc->file_table[i] = NULL;
+		kfree(curproc->p_ft[i]);
+		curproc->p_ft[i] = NULL;
 		return err;
+	}
+	switch(mode){
+		case O_RDONLY:
+			curproc->p_ft[i]->flag = O_RDONLY;
+			break;
+		case O_WRONLY:
+			curproc->p_ft[i]->flag = O_WRONLY;
+			break;
+		case O_RDWR:
+			curproc->p_ft[i]->flag = O_RDWR;
+			break;
+		default:
+			vfs_close(curproc->p_ft[i]->vnode);
+			kfree(curproc->p_ft[i]);
+			curproc->p_ft[i] = NULL;
+			return EINVAL;
 	}
 	if(flags & O_APPEND){
 		struct stat statbuf;
-		err = VOP_STAT(curproc->file_table[i]->vnode, &statbuf);
+		err = VOP_STAT(curproc->p_ft[i]->vnode, &statbuf);
 		if (err){
 			kfree(curproc->file_table[i]);
 			curproc->file_table[i] = NULL;
@@ -52,22 +73,7 @@ int sys_open(const char *filename, int flags, int *retval) {
 	curproc->file_table[i]->destroy_count = 1;
 	curproc->file_table[i]->con_file = false;
 	mode = flags & O_ACCMODE;
-	switch(mode){
-		case O_RDONLY:
-			curproc->file_table[i]->mode_open = O_RDONLY;
-			break;
-		case O_WRONLY:
-			curproc->file_table[i]->mode_open = O_WRONLY;
-			break;
-		case O_RDWR:
-			curproc->file_table[i]->mode_open = O_RDWR;
-			break;
-		default:
-			vfs_close(curproc->file_table[i]->vnode);
-			kfree(curproc->file_table[i]);
-			curproc->file_table[i] = NULL;
-			return EINVAL;
-	}
+	
 	curproc->file_table[i]->lock = lock_create("filehandle_lock");
 	if(curproc->file_table[i]->lock == NULL) {	
 		vfs_close(curproc->file_table[i]->vnode);
