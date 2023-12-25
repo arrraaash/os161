0a356ad804aabaa66e8a1308cc7bd4bb47322fd8
arrraaash <arash.amini.71@gmail.com>
2023-12-25 18:34:18 +0100

    modify sys_open

--- src/kern/syscall/file_syscalls.c
+++ src/kern/syscall/file_syscalls.c
@@ -39,14 +39,15 @@ int sys_open(const char *filename, int flags, mode_t mode, int *retval) {
 	curproc->p_ft[i] = (struct file_handle *)kmalloc(sizeof(struct file_handle));
 	KASSERT(curproc->p_ft[i] != NULL);
 	//complete explanatin is in /vfs/vfspath.c
-	err = vfs_open(checked_fn, flags, 0, &curproc->p_tf[i]->vnode);
+	err = vfs_open(checked_fn, flags, mode, &curproc->p_tf[i]->vnode);
 	// destroy the created space and file table in case of error
 	if (err) {
 		kfree(curproc->p_ft[i]);
 		curproc->p_ft[i] = NULL;
 		return err;
 	}
-	switch(mode){
+	how = flags & O_ACCMODE;  //take from implementation of vfs_open()
+	switch(how){
 		case O_RDONLY:
 			curproc->p_ft[i]->flag = O_RDONLY;
 			break;
@@ -62,7 +63,7 @@ int sys_open(const char *filename, int flags, mode_t mode, int *retval) {
 			curproc->p_ft[i] = NULL;
 			return EINVAL;
 	}
-	if(flags & O_APPEND){  // if there is append the difference is we have to continue writing/reading from a last index
+	if (openflags & O_APPEND){  // if there is append the difference is we have to continue writing/reading from a last index
 		curproc->p_ft[i]->offset = offset + 1; //not sure just have to try
 	} else {
 		curproc->p_ft[i]->offset = 0;
@@ -73,12 +74,13 @@ int sys_open(const char *filename, int flags, mode_t mode, int *retval) {
 	checking the name of file --> done
 	checking the mode --> done
 	considering append --> done
-	considering creat --> creat if not exist
-	how to know it exists
-	considering excl -->
-	considering trun -->
-
+	considering creat --> implemented in vfs_open()
+	considering excl --> implemented in vfs_open()
+	considering trun --> implemented in vfs_open()
 	*/
+	// i just configur vars and flages which are allocated in mem -> things we have to know about our file !!!!!!!
+	//  but actuall work is done in vfs_open()
+	
 	if(curproc->p_ft[i]->lock == NULL) {	
 		vfs_close(curproc->p_ft[i]->vnode);
 		kfree(curproc->p_ft[i]);
